acc71b3c289720bf7633fdaa0e898dab97f95d23
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-07-07 23:22:33 +0900

    add func get random codes

--- app/src/main/java/com/example/encryptedcontacttracing/MainActivity.kt
+++ app/src/main/java/com/example/encryptedcontacttracing/MainActivity.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
+import java.util.*
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +26,8 @@ class MainActivity : AppCompatActivity() {
         qrScanIntegrator.setOrientationLocked(false)
 
         btnGetQR.setOnClickListener {
-            val data = qrScanIntegrator.initiateScan()
+//            val data = qrScanIntegrator.initiateScan()
+            getEncryptCodes(1234)
         }
     }
 
@@ -31,5 +36,13 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == IntentIntegrator.REQUEST_CODE) {
             val result : IntentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         }
+    }
+
+    fun getEncryptCodes(code:Int) {
+        val testLabel: TextView = findViewById(R.id.test)
+        val time = System.currentTimeMillis()
+        val seed = time * code
+        val randomCode = Random(seed).nextLong()
+        testLabel.text = randomCode.toString()
     }
 }

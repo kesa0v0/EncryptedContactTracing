47b7c4da46ccf68dddc8e39df68103633c166fe4
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-07-12 01:47:11 +0900

    disable removal and add intent to mainactivity

--- app/src/main/java/com/example/encryptedcontacttracing/MainActivity.kt
+++ app/src/main/java/com/example/encryptedcontacttracing/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.encryptedcontacttracing
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -15,8 +16,9 @@ import com.google.zxing.integration.android.IntentResult
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
-import java.nio.channels.NotYetBoundException
 import java.util.*
+
+
 
 
 
@@ -40,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         btnGetQR.setOnClickListener {
             //            val data = qrScanIntegrator.initiateScan()
 //            getEncryptCodes(1234)
-            NotifyRecording()
+            notifyRecording()
         }
     }
 
@@ -113,14 +115,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun NotifyRecording() {
+    private fun notifyRecording() {
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        notificationIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
         val builder = NotificationCompat.Builder(this, "10001")
             .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle("test")
-            .setContentText("teststes")
+            .setContentTitle("다중이용시설에 있습니다")
+            .setContentText("다중이용시설에 있습니다. 나가실 때 꺼주세요.")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setOngoing(true)
 
         val channel = NotificationChannel("10001", "Test", NotificationManager.IMPORTANCE_HIGH)
         notificationManager.createNotificationChannel(channel)

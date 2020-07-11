c945feae5920b801ef5490665713858ea97dfbb0
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-07-11 23:18:51 +0900

    moved PeriodicTimer?

--- app/src/main/java/com/example/encryptedcontacttracing/MainActivity.kt
+++ app/src/main/java/com/example/encryptedcontacttracing/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.os.postDelayed
 
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
@@ -29,9 +31,23 @@ class MainActivity : AppCompatActivity() {
         val qrScanIntegrator = IntentIntegrator(this)
         qrScanIntegrator.setOrientationLocked(false)
 
+        val handler =  Handler()
+        val delayMillis:Long = 300000
+// Define the code block to be executed
+        val runnableCode = Runnable {
+            @Override
+            fun run() {
+                // Do something here on the main thread
+                // Repeat this the same runnable code block again another 2 seconds
+                // 'this' is referencing the Runnable object
+                handler.postDelayed(this, delayMillis)
+            }
+        }
+
         btnGetQR.setOnClickListener {
             //            val data = qrScanIntegrator.initiateScan()
             getEncryptCodes(1234)
+            handler.post(runnableCode)
         }
     }
 

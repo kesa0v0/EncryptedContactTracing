0ddc861bca3c02204b852e580571694db8742696
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-07-15 23:03:13 +0900

    add ShowCodes Activity

---
+++ app/src/main/java/com/example/encryptedcontacttracing/ShowCodes.kt
@@ -0,0 +1,12 @@
+package com.example.encryptedcontacttracing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowCodes : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_codes)
+    }
+}
